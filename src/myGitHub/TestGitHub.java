dfd9f005a5d6cb98a18bb95ecd263a575f6d379f
mikiyasT <write2miki@gmail.com>
2015-12-01 17:55:40 -0600

    mySeocondCommit

---
+++ src/myGitHub/TestGitHub.java
@@ -0,0 +1,10 @@
+package myGitHub;
+
+public class TestGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a github test");
+	}
+
+}
