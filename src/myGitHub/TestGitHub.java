5f17ed8b25f79e5877737e81690b78b627aa8b6d
mikiyasT <write2miki@gmail.com>
2015-12-01 18:25:08 -0600

    Testing GitHub again

--- src/myGitHub/TestGitHub.java
+++ src/myGitHub/TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a github test pampampampam  ohhh ");
+		System.out.println("This is a github test pampampampam----  ohhh ");
 		
 	}
 
